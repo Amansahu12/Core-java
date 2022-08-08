9cf2c5b035086f22f00cdc089e91e4e6c3aa1893
Amansahu12 <amansahu9853975@gmail.com>
2022-08-08 15:24:49 +0530

    msg

---
+++ basic/src/in/co/rays/Demo.java
@@ -0,0 +1,17 @@
+package in.co.rays;
+
+public class Demo {
+	
+	
+public static void main(String[] args) {
+		
+		int a=34;
+		int b =5342;
+		int c=a+b;
+		int d=a+b+c;
+		System.out.println(d);
+		
+		
+	}
+
+}
